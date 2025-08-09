2c9332938ac2138c8a032906236af202470e124d
0-s-0 <xushicai@proton.me>
2025-08-09 11:07:32 +0200

    初始化项目

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- head -->
+  <div >
+     <NuxtImg src="https://example.com/photo.jpg" width="600" height="400" alt="Example photo" />
+  </div>
+</template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title:"Auto Perionice"
+})
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,31 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+export default defineNuxtConfig({
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/test-utils",
+  ],
+  css: ["~/assets/css/main.css", "~/assets/css/base.css"],
+  postcss: {
+    plugins: {
+      "postcss-pxtorem": {
+        rootValue: 16, // 指定转换倍率，我现在设置这个表示1rem=37.5px;
+        propList: ["*"], // 属性列表，表示你要把哪些css属性的px转换成rem，这个*表示所有
+        selectorBlackList: ["html"],
+        mediaQuery: false, // 是否允许使用媒体查询，false媒体查询的代码可用，true不可用
+        exclude: "ignore",
+        replace: true, // 替换包含rem的规则，而不是添加回退
+        minPixelValue: 1, // 需要转换的最小值，一般1px像素不转换，以上才转换
+        unitPrecision: 6, // 转换成rem单位的小数点后的保留位数
+      },
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
